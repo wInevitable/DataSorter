6bd124a07c39004924d8acc49034828b5c0e1746
William Noll <williampnoll@gmail.com>
2015-06-03 22:39:02 -0400

    Add Delimiters and Data Variables

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,22 +1,27 @@
 class DataSorter
   # Initializes the DataSorter data attribute from specific files
-  def initialize
-    @data
+  def initialize(*files)
+    @data = ''
+    # Add/Remove delimiters as necessary, Space is Default
+    @delimiters = [',', '|']
+
+    files.each do |f|
+    end
   end
 
   # Sorts by Gender (females before males) and then Last Name (ascending)
   def gender_sort
-
+    return @data
   end
 
   # Sorts by Date of Birth (ascending)
   def dob_sort
-
+    return @data
   end
 
   # Sorts by Last Name (descending)
   def last_name_sort
-
+    return @data
   end
 
   private

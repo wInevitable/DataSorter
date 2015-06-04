83a7823312a8eb156c3faf8f445df3416a3093bc
William Noll <williampnoll@gmail.com>
2015-06-03 22:30:50 -0400

    Adjust rspec, Write Tests for Data Sorter

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,1 +1,27 @@
-#ruby
+class DataSorter
+  # Initializes the DataSorter data attribute from specific files
+  def initialize
+    @data
+  end
+
+  # Sorts by Gender (females before males) and then Last Name (ascending)
+  def gender_sort
+
+  end
+
+  # Sorts by Date of Birth (ascending)
+  def dob_sort
+
+  end
+
+  # Sorts by Last Name (descending)
+  def last_name_sort
+
+  end
+
+  private
+  # Parses individual files based off of chosen delimiters
+  def parser(file)
+
+  end
+end
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,18 +1,42 @@
 require 'rspec'
 require 'challenge'
 
-describe "" do
-  # test code
+describe DataSorter do
+  subject(:sorter) { DataSorter.new('comma_delimited', 'pipe_delimited', 'space_delimited') }
 
-  it "" do
-    #code
+  it "sorts by Gender (females before males) and then Last Name ascending" do
+    expect(sorter.gender_sort).to eq('Hingis Martina Female 4/2/1979 Green
+      Kelly Sue Female 7/12/1959 Pink
+      Kournikova Anna Female 6/3/1975 Red
+      Seles Monica Female 12/2/1973 Black
+      Abercrombie Neil Male 2/13/1943 Tan
+      Bishop Timothy Male 4/23/1967 Yellow
+      Bonk Radek Male 6/3/1975 Green
+      Bouillon Francis Male 6/3/1975 Blue
+      Smith Steve Male 3/3/1985 Red')
   end
 
-  it "" do
-    #code
+  it "sorts by Birth Date ascending" do
+    expect(sorter.dob_sort).to eq('Abercrombie Neil Male 2/13/1943 Tan
+      Kelly Sue Female 7/12/1959 Pink
+      Bishop Timothy Male 4/23/1967 Yellow
+      Seles Monica Female 12/2/1973 Black
+      Bonk Radek Male 6/3/1975 Green
+      Bouillon Francis Male 6/3/1975 Blue
+      Kournikova Anna Female 6/3/1975 Red
+      Hingis Martina Female 4/2/1979 Green
+      Smith Steve Male 3/3/1985 Red')
   end
 
-  it "" do
-    #code
+  it "sorts by Last Name descending" do
+    expect(sorter.last_name_sort).to eq('Smith Steve Male 3/3/1985 Red
+      Seles Monica Female 12/2/1973 Black
+      Kournikova Anna Female 6/3/1975 Red
+      Kelly Sue Female 7/12/1959 Pink
+      Hingis Martina Female 4/2/1979 Green
+      Bouillon Francis Male 6/3/1975 Blue
+      Bonk Radek Male 6/3/1975 Green
+      Bishop Timothy Male 4/23/1967 Yellow
+      Abercrombie Neil Male 2/13/1943 Tan')
   end
 end

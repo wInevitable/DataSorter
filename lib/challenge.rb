18860a19c76707ef90e051210f18f8621ab16945
William Noll <williampnoll@gmail.com>
2015-06-07 22:07:09 -0400

    Refactor Render Method and Default Delimiter. Store Parsed Data in a Hash instead of an Array

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -2,57 +2,91 @@ class DataSorter
   # Initializes the DataSorter data attribute from specific files
   def initialize(*files)
     # Add/Remove delimiters as necessary, Space is Default
+    @default_delimiter = ' '
     @delimiters = [',', '|']
     @data = []
 
+    # Read through each File with the Parser
+    # Returns an Array of Hashes [{Last name: 'x', ...}, {}, {}]
     files.each do |f|
-      @data += parser(f) # returns arr of arrays => [[df_1], [df_2], ...]
+      @data += parser(f)
     end
   end
 
-  # Data ~= [LName, FName, Gender, DOB, Color]
-  # Sorts by Gender (females before males) and then Last Name (ascending)
-  def gender_sort # [Index 2, then Index 0]
-    sorted_array = @data.sort do |a, b|
-      comp = (a[2] <=> b[2])
-      comp.zero? ? (a[0] <=> b[0]) : comp
-    end
+  # Place Any Desired Sorters Below
+  # Each is Responsible for Sorting the Parsed Data
+  # And Passing it to the Render Function
 
-    sorted_array.map! { |arr| arr.join(' ') }
-    return sorted_array.join("\n")
-  end
-
-  # Sorts by Date of Birth (ascending) [Index 3]
+  # Sorts by Date of Birth (ascending)
   def dob_sort
     sorted_array = @data.sort do |a, b|
-      date_a = a[3].split('/')
-      date_b = b[3].split('/')
+      date_a = a[:date].split('/')
+      date_b = b[:date].split('/')
 
+      # Sort by Year first, then Month, then Day
       comp = (date_a[2] <=> date_b[2])
       comp = comp.zero? ? (date_a[0] <=> date_b[0]) : comp
       comp.zero? ? (date_a[1] <=> date_b[1]) : comp
     end
 
-    sorted_array.map! { |arr| arr.join(' ') }
-    return sorted_array.join("\n")
+    render(sorted_array)
   end
 
-  # Sorts by Last Name (descending) [Index 0]
+  # Sorts by Gender (females before males) and then Last Name (ascending)
+  def gender_sort
+    sorted_array = @data.sort do |a, b|
+      comp = (a[:gender] <=> b[:gender])
+      comp.zero? ? (a[:last_name] <=> b[:last_name]) : comp
+    end
+
+    render(sorted_array)
+  end
+
+  # Sorts by Last Name (descending)
   def last_name_sort
-    sorted_array = @data.sort_by { |arr| arr[0] }
-    sorted_array = sorted_array.reverse.map! { |arr| arr.join(' ') }
-    return sorted_array.join("\n")
+    sorted_array = @data.sort_by { |hash| hash[:last_name] }
+    render(sorted_array.reverse)
   end
 
   private
-  # Parses individual files based off of chosen delimiters
-  # Returns an Array of Lines
+  # Should Return a Hash with Information
+  def clean_data(arr)
+    formatted_data = {}
+
+    arr.delete_at(2) if arr.length > 5 # drop initials
+
+    arr.each_with_index do |el, i|
+      el.gsub!(/\s+/, '') # Remove any remaining whitespace/new line characters
+
+      case i
+      when 0
+        formatted_data[:last_name] = el
+      when 1
+        formatted_data[:first_name] = el
+      when 2
+        formatted_data[:gender] = el[0].upcase == 'M' ? 'Male' : 'Female'
+      else
+        if el.include?('/')
+          formatted_data[:date] = el
+        elsif el.include?('-')
+          formatted_data[:date] = el.gsub('-', '/')
+        else # Handle Colors
+          formatted_data[:fav_color] = el
+        end
+      end
+    end
+
+    formatted_data
+  end
+
+  # Handles File Input, splitting File Lines into Arrays via chosen delimiters
+  # Returns an Array of Hashes/Data Objects [{Last name: 'x', ...}, {}, {}]
   def parser(file)
-    delimiter = ' '
+    delimiter = @default_delimiter
     parsed_file = []
     input_rows = IO.readlines('data/' + file)
 
-    # Pick File-Specific Delimiter
+    # Choose File-Specific Delimiter
     @delimiters.each do |d|
       if input_rows[0].include?(d)
         delimiter = d
@@ -68,35 +102,16 @@ class DataSorter
     parsed_file
   end
 
-  def clean_data(arr)
+  # Handles Program Output
+  # Takes Sorted Data and Returns (or optionally prints) in Desired Order
+  def render(sorted_array)
+    # Orders each specific Object in attribute order
     # Desired Order = [LName, FName, Gender, DOB, Color]
-    formatted_array = []
-
-    arr.delete_at(2) if arr.length > 5 # drop initials
-
-    arr.each_with_index do |el, i|
-      el.gsub!(' ', '')
-      el.gsub!("\n", '')
-
-      if i < 2
-        formatted_array << el # Handle Last and First Names
-      elsif i == 2
-        if el[0].upcase == 'M' # Handle Gender
-          formatted_array << 'Male'
-        else
-          formatted_array << 'Female'
-        end
-      else
-        if el.include?('/')
-          formatted_array[3] = el
-        elsif el.include?('-')
-          formatted_array[3] = el.gsub('-', '/')
-        else # Handle Colors
-          formatted_array[4] = el
-        end
-      end
+    sorted_array.map! do |hash|
+      hash = hash[:last_name] + " " + hash[:first_name] + " " + hash[:gender] + " " + hash[:date] + " " + hash[:fav_color]
     end
 
-    formatted_array
+    # Joins all Objects with new lines
+    return sorted_array.join("\n")
   end
 end
